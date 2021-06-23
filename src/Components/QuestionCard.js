79740e2aaf994a7e7a9ffd3f12550555b1d5422c
stephane-duteriez <stephane.duteriez@gmail.com>
2021-06-23 08:39:17 +0200

    feature: improving cards

--- src/Components/QuestionCard.js
+++ src/Components/QuestionCard.js
@@ -1,9 +1,13 @@
-import CardHeader from '@material-ui/core/CardHeader';
-import {makeStyles} from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import Avatar from '@material-ui/core/Avatar';
 import React from 'react';
+import CardHeader from '@material-ui/core/CardHeader';
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import {useSelector} from 'react-redux';
 
 const useStyle = makeStyles((theme) => ({
@@ -11,8 +15,17 @@ const useStyle = makeStyles((theme) => ({
     width: 345,
   },
   avatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+    margin: 10,
+  },
+  image: {
+    padding: 10,
+    width: theme.spacing(12),
+    heigt: theme.spacing(12),
+  },
+  button: {
+    width: '100%',
   },
 }));
 
@@ -22,14 +35,15 @@ const useStyle = makeStyles((theme) => ({
  * @return {Component}
  */
 export default function QuestionCard(props) {
-  const {userName, alreadyAnswered, userAvatar} =
-    useSelector(({questions, users, authedUser}) => {
+  const {userName, userAvatar, optionOne} =
+    useSelector(({questions, users}) => {
       const question = questions[props.idQuestion];
       return {
         userName: users[question.author].name,
         userAvatar: users[question.author].avatarURL,
-        alreadyAnswered: authedUser?users[authedUser].answers[props.idQuestion]:
-          false,
+        optionOne: question.optionOne.text,
+        // alreadyAnswered: authedUser?users[authedUser]
+        // .answers[props.idQuestion]:false,
       };
     });
 
@@ -37,15 +51,34 @@ export default function QuestionCard(props) {
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={
-          <Avatar
-            aria-label={`avatar of ${userName}`}
-            className={classes.avatar}
-            src={userAvatar}/>
-        }
-        title={`somebody's name ${userName}`}
-        subheader={alreadyAnswered?'Answered':'NotAnswered'}
+        title={`${userName} asked`}
       />
+      <CardContent >
+        <Grid container spacing={2}>
+          <Grid item className={classes.image}>
+            <Avatar
+              aria-label={`avatar of ${userName}`}
+              className={classes.avatar}
+              src={userAvatar}/>
+          </Grid>
+          <Grid item xs={12} sm>
+            <Typography variant="h6" gutterBottom>
+              Would you rather
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              {`...${optionOne}...`}
+            </Typography>
+            <Button
+              variant="outlined"
+              color="primary"
+              size="small"
+              className={classes.button}
+              href={`/question/${props.idQuestion}`}>
+              View Pull
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
