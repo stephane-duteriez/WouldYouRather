830bf9c5e8b4b23ca39d5bc534595fc91301cc38
stephane-duteriez <stephane.duteriez@gmail.com>
2021-07-06 21:55:25 +0200

    style: change order import mygrid

--- src/Components/MyGrid.js
+++ src/Components/MyGrid.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/core/styles';
-import {Grid} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
-const useStyle = makeStyles((theme) => ({
+const useStyle = makeStyles({
   myGrid: {
     paddingTop: '1em',
   },
-}));
+});
 
 /**
  * @descrition Component list of question
